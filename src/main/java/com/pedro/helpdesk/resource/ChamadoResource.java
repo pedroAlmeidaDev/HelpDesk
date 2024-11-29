9cc7933ba4aa25ea2478fbacd2aff3410fdceecc
pedroAlmeidaDev <p_henrique1182@hotmail.com>
2024-11-28 23:41:40 -0400

    Ajustes no findById dos Chamados

--- src/main/java/com/pedro/helpdesk/resource/ChamadoResource.java
+++ src/main/java/com/pedro/helpdesk/resource/ChamadoResource.java
@@ -2,6 +2,7 @@ package com.pedro.helpdesk.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ChamadoResource {
 	@Autowired
 	private ChamadoService service;
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
